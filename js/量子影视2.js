8a19697976097af399883ba605255a9cc40cc622
sqspot <111124994+sqspot@users.noreply.github.com>
2022-10-24 09:54:06 +0800

    Add files via upload

---
+++ js/量子影视2.js
@@ -0,0 +1,8 @@
+var rule = Object.assign(muban.mxone5,{
+//模板:'mxone5',
+title:'量子影视2',
+host:'http://www.lzizy9.com',
+url:'/index.php/vod/show/id/fyclass.html',
+searchUrl:'/index.php/vod/search.html?wd=**',
+cate_exclude:'网址'
+});
