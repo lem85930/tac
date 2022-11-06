46595627936940cd82016940da8518efa325cc05
sqspot <111124994+sqspot@users.noreply.github.com>
2022-11-06 12:54:55 +0800

    Add files via upload

--- js/电影大师.js
+++ js/电影大师.js
@@ -3,11 +3,5 @@ title:'电影大师',
 host:'https://dyds8.cyou',
 url:'/index.php/vod/type/id/fyclass/page/fypage.html',
 searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
-class_parse:'.stui-header__menu&&li;a&&Text;a&&href;.*/(.*?).html',
-推荐:'.stui-vodlist;li;h4.title&&Text;.lazyload&&data-original;.pic-text&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
-二级:{"title":"h1&&Text;.stui-content__detail p;eq(0)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(3)&&Text","content":".detail-content&&Text","tabs":"ul.nav-tabs&&li","lists":".stui-content__playlist:eq(#id) li"},
-搜索:'.stui-vodlist&&li;h4.title&&Text;.lazyload&&data-original;.pic-text&&Text;a&&href',
-
+class_parse:'.dropdown.type.clearfix li:gt(2):lt(6);a&&Text;a&&href;.*/(.*?).html',
 });
