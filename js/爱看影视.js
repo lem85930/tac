03d664030614db579c3a32eaa6e9533c0d478f00
sqspot <111124994+sqspot@users.noreply.github.com>
2022-11-06 13:00:37 +0800

    Add files via upload

--- js/爱看影视.js
+++ js/爱看影视.js
@@ -1,23 +1,16 @@
-var rule={
-    title:'爱看影视',
-    host:'https://ikan6.vip/',
-    // homeUrl:'/',
-    url:'/vodtype/fyclass-fypage/',
-    searchUrl:'/vodsearch/-------------/?wd=**&submit=',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
-    },
-    class_name:'电影&连续剧&综艺&动漫&日韩剧&欧美剧',
-    class_url:'1&2&3&4&15&16',
-    lazy:'',
-    limit:6,
-    推荐:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":"h1&&Text;.left&&Text","img":".lazyload&&data-original","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".data.hidden-xs&&Text","tabs":".nav.nav-tabs.active:eq(0)","lists":".col-lg-10:eq(#id) a"},
-    搜索:'.myui-vodlist__media.clearfix;a&&title;.lazyload&&data-original;.pic-text&&Text',
+var rule={     
+    title:'爱看影视',     
+    host:'https://ikan6.vip',     
+    url:'/vodtype/fyclass-fypage/ ',    
+    searchUrl:'/vodsearch/-------------/?wd=**&submit=', 
+    searchable:2,
+    quickSearch:1,
+    filterable:0,
+    headers:{'User-Agent':'MOBILE_UA',         },     
+    class_name:'电影&电视剧&综艺&动漫&日韩剧&美剧',     
+    class_url:'1&2&3&4&15&16',     
+    推荐:'.myui-vodlist__box;*;*;*;*',     
+    一级:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',     
+    二级:{"title":"h1&&Text;.myui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":";;;.myui-content__detail p:eq(3)&&Text;.myui-content__detail p:eq(4)&&Text","content":".sketch.content&&Text","tabs":'.nav.nav-tabs.active',"lists":".myui-content__list:eq(#id) a"},     
+    搜索:'.myui-vodlist__media.clearfix li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href', 
 }
