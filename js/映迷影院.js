d2bd4d17f4bfa509f9c955eafff96778d27222d8
sqspot <111124994+sqspot@users.noreply.github.com>
2022-12-13 23:38:38 +0800

    Add files via upload

--- js/映迷影院.js
+++ js/映迷影院.js
@@ -5,7 +5,7 @@ var rule = {
     url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
-    quickSearch:1,//是否启用快速搜索,
+    quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
     headers:{//网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent':'MOBILE_UA',
