61cfb7b8444f54ee80c137106939bd2ea6e0e4fe
sqspot <111124994+sqspot@users.noreply.github.com>
2022-12-13 23:32:37 +0800

    Add files via upload

--- js/艾旦影视.js
+++ js/艾旦影视.js
@@ -3,9 +3,5 @@ title:'艾旦影视',
 host:'https://www.lovedan.net',
 url:'/vodshow/fyclass--------fypage---.html',
 searchUrl:'/vodsearch/**----------fypage---.html',
-class_name:'電影&電視劇&綜藝&動漫',
-class_url:'1&2&3&4',
-cate_exclude: '电视剧|综艺|动漫|福利视频|福利图片|最新|排行',
-
-
+cate_exclude: '福利图片|最新|排行',
 });
