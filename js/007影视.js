31f650795b003e7d8ab221812fbcd1ae22d8263a
sqspot <i20110323@gmail.com>
2023-08-06 11:55:43 +0800

    up

--- js/007影视.js
+++ js/007影视.js
@@ -3,6 +3,8 @@ muban.首图2.二级.desc = '.data:eq(1)&&Text;;;.data:eq(6)&&Text;.data:eq(5)&&
 muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
 var rule = {
     title:'007影视',
+    搜索编码:'utf-8',
+    // 搜索编码:'gbk',
     模板:'首图2',
     ali_token:'{{ali_token}}',
     bili_cookie:'{{bili_cookie}}',
@@ -22,10 +24,24 @@ var rule = {
         "20":[{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]
     },
     class_parse: '.stui-header__menu li:gt(0):lt(9);a&&Text;a&&href;.*/(.*?).html',
-    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);log(html);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else if(/qq|iqiyi/.test(url)){input={jx:0,url:'https://jx.m3u8.tv/jiexi/?url='+url,parse:1,header:JSON.stringify({'user-agent':'Mozilla/5.0'})}}else{input}",
+    lazy:`js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+    log(html);
+    var url=html.url;
+    if(html.encrypt=='1'){
+    url=unescape(url)
+    }else if(html.encrypt=='2'){
+    url=unescape(base64Decode(url))
+    }
+    if(/m3u8|mp4/.test(url)){
+    input=url
+    }else if(/qq|iqiyi/.test(url)){
+    input={jx:0,url:'https://jx.m3u8.tv/jiexi/?url='+url,parse:1,header:JSON.stringify({'user-agent':'Mozilla/5.0'})}
+    }else{
+    input
+    }`,
 
     // searchUrl:'/search/**----------fypage---.html',
-    searchUrl:'/index.php/ajax/suggest?mid=1&wd=**',
+    searchUrl:'/index.php/ajax/suggest?mid=1&wd=**&limit=50',
     detailUrl:'/detail/fyid.html', //非必填,二级详情拼接链接
     搜索:'json:list;name;pic;;id',
 }
--- js/嘶哩嘶哩.js
+++ js/嘶哩嘶哩.js
@@ -1,6 +1,7 @@
 var rule = {
     title:'嘶哩嘶哩',
-    host:'https://www.silisili.tv',
+    // host:'https://www.silisili.tv',
+    host:'https://www.silisilifun.com',
     // url:'/vodtype/fyclass-fypage/',
     url:'/vodtype/fyclass-fypagefyfilter/',
     filterable:1,//是否启用分类筛选,
@@ -21,7 +22,8 @@ var rule = {
     play_parse:true,
     lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
     limit:6,
-    推荐:'.sliderlist.vod_hl&&.vod_hl_list;.list-body&&Text;.thumb&&style;.list-footer&&div--time&&Text;*',
+    // 推荐:'.sliderlist.vod_hl&&.vod_hl_list;.list-body&&Text;.thumb&&style;.list-footer&&div--time&&Text;*',
+    推荐:'.row.wall&&.post;*;.scrollLoading&&data-url;*;*',
     一级:'#content article ;.entry-title&&a--span&&Text;img&&src;.entry-meta--time&&Text;a&&href',
     二级:{
         "title":"h1&&Text;.data--span:eq(0)&&Text",
