457f7f071fdfdd10e983bd4d0daacd39fdf89540
sqspot <i20110323@gmail.com>
2024-02-26 23:04:37 +0800

    up

--- TV倉.js
+++ TV倉.js
@@ -1,4 +1,5 @@
-[
+{
+"urls":[
 {"name":"xxx","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx.m3u"},
 {"name":"xxx Japan 1","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx Japan 1.m3u"},
 {"name":"xxx Japan 2","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx Japan 2.m3u"},
@@ -6,4 +7,4 @@
 {"name":"xxx Japan F-L","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx Japan F-L.m3u"},
 {"name":"xxx Japan M-S","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx Japan M-S.m3u"},
 {"name":"xxx Japan T-Z","url":"https://raw.kgithub.com/eklins/FDTV/main/tv/xxx Japan T-Z.m3u"}
-]
+]}
