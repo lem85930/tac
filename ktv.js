ea54f0f58a2cdebab2e7eba745920b7f8648833c
sqspot <i20110323@gmail.com>
2024-03-04 21:59:42 +0800

    uo

--- ktv.js
+++ ktv.js
@@ -1,11 +1,10 @@
 
 {
 "spider":"./pg.jar",
-//"spider":"http://maoyingshi.cc/tvbox/云星日记/jar/bili.jar",
 "sites":[
 {"key":"MV_vod","name":"MTV合集","type":1,"api":"https://tvbox-mv-xianlaihuyu.koyeb.app/mv/vod","searchable":1,"quickSearch":1,"filterable":1,"changeable":0},
 {"key":"Iktv","name":"KTV","type":3,"api":"csp_Iktv","searchable":1,"changeable":0},
-{"key":"MV_vod","name":"电视┃MTV","type":1,"api":"https://mv.wogg.link/mv/vod","searchable":1,"quickSearch":0,"changeable":0},
+{"key":"MV_vod2","name":"电视┃MTV","type":1,"api":"https://mv.wogg.link/mv/vod","searchable":1,"quickSearch":0,"changeable":0},
 {"key":"Yinyuetai","name":"音悦台","type":3,"api":"csp_Yinyuetai","searchable":1,"changeable":0}
 ]
 
