907ce50938b604e664ce9ca58a35ef0902a9ccd4
sqspot <i20110323@gmail.com>
2024-03-04 23:18:47 +0800

    UP

--- ktv.js
+++ ktv.js
@@ -1,11 +1,14 @@
-
+// https://github.com/leosam1024/tvbox-mv
 {
 "spider":"./pg.jar",
+"lives":[{"name":"KTV","type":0,"url":"./KTV.txt"}],
 "sites":[
-{"key":"MV_vod","name":"MTV合集","type":1,"api":"https://tvbox-mv-xianlaihuyu.koyeb.app/mv/vod","searchable":1,"quickSearch":1,"filterable":1,"changeable":0},
-{"key":"Iktv","name":"KTV","type":3,"api":"csp_Iktv","searchable":1,"changeable":0},
-{"key":"MV_vod2","name":"电视┃MTV","type":1,"api":"https://mv.wogg.link/mv/vod","searchable":1,"quickSearch":0,"changeable":0},
-{"key":"Yinyuetai","name":"音悦台","type":3,"api":"csp_Yinyuetai","searchable":1,"changeable":0}
+{"key":"MV_vod","name":"MTV合集","type":1,"api":"https://tvbox-mv-xianlaihuyu.koyeb.app/mv/vod","searchable":1,"quickSearch":1,"filterable":1,"changeable":0,"playerType":"1"},
+{"key":"Iktv","name":"KTV","type":3,"api":"csp_Iktv","searchable":1,"changeable":0,"playerType":"1"},
+{"key":"MV_vod2","name":"电视┃MTV","type":1,"api":"https://mv.wogg.link/mv/vod","searchable":1,"quickSearch":0,"changeable":0,"playerType":"1"},
+{"key":"MV_vod3","name":"🎸┃明星-接口1┃MV","type":1,"api":"https://tvbox-mv--leosam2048.repl.co/mv/vod","searchable":1,"quickSearch":1,"filterable":1,"playerType":"1"},
+{"key":"MV_vod4","name":"🎸┃明星-接口2┃MV","type":1,"api":"https://mv.饭太硬.ml/mv/vod","searchable":1,"quickSearch":1,"filterable":1,"playerType":"1"},
+{"key":"Yinyuetai","name":"音悦台","type":3,"api":"csp_Yinyuetai","searchable":1,"changeable":0,"playerType":"1"}
 ]
 
 }
